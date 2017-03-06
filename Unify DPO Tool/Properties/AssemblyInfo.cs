2cf186e8b373a7056b5522ef2d2eb1a74898539e
poxonline <pP.10011996>
2017-03-06 20:19:28 +0100

    Versions anapssung

--- Unify DPO Tool/Properties/AssemblyInfo.cs
+++ Unify DPO Tool/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5.0")]
-[assembly: AssemblyFileVersion("0.0.5.0")]
+[assembly: AssemblyVersion("0.0.6.0")]
+[assembly: AssemblyFileVersion("0.0.6.0")]
 [assembly: NeutralResourcesLanguageAttribute("de-DE")]
