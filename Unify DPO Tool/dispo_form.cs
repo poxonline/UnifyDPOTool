1b9a258329d465ec7bba12afc5a8e209314ac36d
poxonline <peter.online.gallery@googlemail.com>
2017-07-22 22:52:06 +0200

    Dispos abfragen und Form aktualisierung

--- Unify DPO Tool/dispo_form.cs
+++ Unify DPO Tool/dispo_form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Unify_DPO_Tool
 {
@@ -15,6 +16,25 @@ namespace Unify_DPO_Tool
         public dispo_form()
         {
             InitializeComponent();
+            aktualisieren();
+        }
+        public void aktualisieren()
+        {
+            cb_dispo_auswahl.Items.Clear();
+            tb_id.Text = "";
+            tb_name.Text = "";
+            tb_Beschreibung.Text = "";
+            tb_email.Text = "";
+            cb_FRU.Checked = false;
+            cb_email_normal.Checked = false;
+            cb_email_eskalation.Checked = false;
+            cb_partner.Checked = false;
+            ArrayList dispoliste = new ArrayList();
+            dispoliste = sql_dispos.select_dispos();
+            foreach(dispos element in dispoliste)
+            {
+                cb_dispo_auswahl.Items.Add(element);
+            }
         }
     }
 }
--- Unify DPO Tool/sql_dispos.cs
+++ Unify DPO Tool/sql_dispos.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Collections;
+using System.Security.Cryptography;
+using System.DirectoryServices;
 
 namespace Unify_DPO_Tool
 {
     class sql_dispos
     {
         //Alle SQL Befehle im Zusammenahng mit Dispos und ASP
-        string connection = sql_aufrufe.connection;
+        static string connection = sql_aufrufe.connection;
+        public static ArrayList select_dispos()
+        {
+            MySqlDataReader rdr = null;
+            using (MySqlConnection verbindung = new MySqlConnection())
+            {
+                try
+                {
+                    verbindung.ConnectionString = connection;
+                    MySqlCommand SQL_Befehl = new MySqlCommand("SELECT * from dispos;", verbindung);
+                    ArrayList liste = new ArrayList();
+                    try
+                    {
+                        verbindung.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    rdr = SQL_Befehl.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        liste.Add(new dispos(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), rdr.GetBoolean(3), rdr.GetBoolean(4), rdr.GetBoolean(5), rdr.GetString(6), rdr.GetBoolean(7)));
+                    }
+                    rdr.Close();
+                    verbindung.Close();
+                    return liste;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Fehler bei der Verbindung mit der Datenbank." + Environment.NewLine + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+                finally
+                {
+                    verbindung.Close();
+                }
+
+            }
+        }
     }
 }
